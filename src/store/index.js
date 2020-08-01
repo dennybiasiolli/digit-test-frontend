b1f6cb41943011a99f99942f9f93997c221c84dd
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2020-08-01 15:45:56 +0200

    src: persisting state to localstorage

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,33 @@ import { configureStore } from '@reduxjs/toolkit';
 import authenticationReducer from './authenticationReducer';
 import counterReducer from '../features/counter/counterSlice';
 
-export default configureStore({
+
+let persistedState;
+try {
+  const serializedState = localStorage.getItem('digit-store-state');
+  if (serializedState === null) {
+    persistedState = undefined;
+  }
+  persistedState = JSON.parse(serializedState) || undefined;
+} catch (err) {
+  persistedState = undefined;
+}
+
+const store = configureStore({
+  preloadedState: persistedState,
   reducer: {
     authentication: authenticationReducer,
     counter: counterReducer,
   },
 });
+
+store.subscribe(() => {
+  try {
+    const serializedState = JSON.stringify(store.getState());
+    localStorage.setItem('digit-store-state', serializedState);
+  } catch {
+    // ignore write errors
+  }
+});
+
+export default store;
